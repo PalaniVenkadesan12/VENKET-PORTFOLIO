7572d68999ab77156954f7a470cba79c2d6c492b
PalaniVenkadesan12 <palanivenkadesan8056@gmail.com>
2024-12-04 12:22:58 +0530

    modify-all

--- script.js
+++ script.js
@@ -1,49 +1,3 @@
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let subject = document.getElementById("subject");
-let message = document.getElementById("message");
-
-(function() {
-  emailjs.init("eAXZUrBjmiWQBgOSd");
-})();
-
-let templateParams = {
-  to_name : "Venkadesan",
-  from_name: name.value,
-  from_email:email.value,
-  from_subject:subject.value,
-  message:message.value
-}
-
-function submitMessage() {
-  templateParams.to_name = "Venkadesan";
-  templateParams.from_name = name.value;
-  templateParams.from_email = email.value;
-  templateParams.from_subject = subject.value;
-  templateParams.message = message.value;
-  emailjs.send("service_fs48a7k","template_h2o5zu6",templateParams)
-  .then(function(){
-    showAlert("Message sent successfully!");
-    name.value="";
-    email.value="";
-    subject.value="";
-    message.value="";
-  }),function() {
-    showAlert("Failed to send message!");
-  }
-}
-
-
- function showAlert(message) {
-   const alert = document.getElementById("alert");
-   alert.innerHTML = message;
-   alert.style.display = "flex";
-   setTimeout(function(){
-     alert.style.display = "none";
-   },3000);
- }
-
-
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
@@ -81,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Python Fullstack Developer", "Data Analyst", "Front-End Developer"],
+        strings: ["Python Fullstack developer", "Data Analyst", "Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-       strings: ["Python Fullstack Developer", "Data Analyst", "Front-End Developer"],
+       strings: ["Python Fullstack developer", "Data Analyst", "Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
